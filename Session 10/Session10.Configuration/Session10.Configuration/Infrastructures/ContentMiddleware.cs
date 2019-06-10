e79dd7e39e4f461b35f2b7cafca72873530933ea
Alireza Oroumand <ar.oroumand@gmail.com>
2019-06-10 20:57:17 +0430

    Sessin 10 MVC Core - Application Configuratino

---
+++ Session 10/Session10.Configuration/Session10.Configuration/Infrastructures/ContentMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session10.Configuration.Infrastructures
+{
+    public class ErrorMiddleware
+    {
+        private RequestDelegate nextDelegate;
+        public ErrorMiddleware(RequestDelegate next)
+        {
+            nextDelegate = next;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await nextDelegate.Invoke(httpContext);
+            if (httpContext.Response.StatusCode == 403)
+            {
+                await httpContext.Response
+                .WriteAsync("Edge not supported", Encoding.UTF8);
+            }
+            else if (httpContext.Response.StatusCode == 404)
+            {
+                await httpContext.Response
+                .WriteAsync("No content middleware response", Encoding.UTF8);
+            }
+        }
+    }
+
+
+    public class ContentMiddleware
+    {
+        private RequestDelegate nextDelegate;
+        private readonly UptimeService uptimeService;
+
+        public ContentMiddleware(RequestDelegate next, UptimeService uptimeService)
+        {
+            nextDelegate = next;
+            this.uptimeService = uptimeService;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Path.ToString().ToLower() == "/middleware")
+            {
+                await httpContext.Response.WriteAsync(
+                $"This is from the content middleware. uptime is {uptimeService.GetUptime}", Encoding.UTF8);
+            }
+            else
+            {
+                await nextDelegate.Invoke(httpContext);
+            }
+        }
+
+    }
+
+}
---
+++ Session 10/Session10.Configuration/Session10.Configuration/Infrastructures/ShortCircuitMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Session10.Configuration.Infrastructures
+{
+    public class ShortCircuitMiddleware
+    {
+        private RequestDelegate nextDelegate;
+        public ShortCircuitMiddleware(RequestDelegate next) => nextDelegate = next;
+        public async Task Invoke(HttpContext httpContext)
+        {
+            bool isEdge = bool.Parse(httpContext.Items["EdgeBrowser"].ToString());
+            if (isEdge)
+            {
+                httpContext.Response.StatusCode = 403;
+            }
+            else
+            {
+                await nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+
+}
