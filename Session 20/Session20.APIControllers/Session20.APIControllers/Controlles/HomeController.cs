fc0d3ca8b7eb0949527473bfa2dce0dba81bafc4
Milad Rashidi <Rashidi.Milad@yahoo.com>
2019-09-17 00:41:06 +0430

    Adding Ajax Call Button For Saving Person In The Memory

---
+++ Session 20/Session20.APIControllers/Session20.APIControllers/Controlles/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Session20.APIControllers.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Session20.APIControllers.Controlles
+{
+    public class HomeController : Controller
+    {
+        private readonly PersonRepository personRepository;
+
+        public HomeController(PersonRepository personRepository)
+        {
+            this.personRepository = personRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(personRepository.GetAll());
+        }
+        public IActionResult AddPerson(Person person)
+        {
+            personRepository.Add(person);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Session 20/Session20.APIControllers/Session20.APIControllers/Controlles/PersonController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Session20.APIControllers.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Session20.APIControllers.Controlles
+{
+    [Route("api/[controller]")]
+    public class PersonController : Controller
+    {
+        private readonly PersonRepository personRepo;
+
+        public PersonController(PersonRepository person)
+        {
+            this.personRepo = person;
+        }
+
+        [HttpGet]
+        public List<Person> Get()
+        {
+            return personRepo.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Person Get(int id)
+        {
+            return personRepo.Get(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Person value)
+        {
+            personRepo.Add(value);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Person value)
+        {
+           
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
