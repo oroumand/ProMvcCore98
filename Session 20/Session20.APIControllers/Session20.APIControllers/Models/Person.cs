be7411f80df130147c713ab2604103588edb5551
oroumand <oroumand@outlook.com>
2019-09-16 18:33:11 +0430

    Session 20

---
+++ Session 20/Session20.APIControllers/Session20.APIControllers/Models/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Session20.APIControllers.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+    public interface PersonRepository
+    {
+        Person Get(int id);
+        void Add(Person person);
+        List<Person> GetAll();
+    }
+    public class FakePersonRepository : PersonRepository
+    {
+        private readonly List<Person> people = new List<Person>
+        {
+            new Person
+            {
+                Id = 1,
+                FirstName = "Alireza",
+                LastName = "Oroumand"
+            },
+            new Person
+            {
+                Id = 2,
+                FirstName="Mahdi",
+                LastName = "Hossini nasab"
+            }
+        };
+        public void Add(Person person)
+        {
+            person.Id = people.Count + 1;
+            people.Add(person);
+        }
+
+        public Person Get(int id)
+        {
+            return people.FirstOrDefault(c => c.Id == id);
+        }
+
+        public List<Person> GetAll()
+        {
+            return people;
+        }
+    }
+}
